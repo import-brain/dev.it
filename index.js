c1b8238b382f8a460f95fb730ea135a32649a168
ericcheng2005 <68196762+import-brain@users.noreply.github.com>
2021-10-18 17:46:54 -0400

    feat: remove node_modules, partial set up of mongoose

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server');
+const graphql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const typeDefs = graphql`
+    type Query{
+        sayHi: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose.connect()
+
+server.listen({ port: 5000 }).then(res => {
+    console.log(`Server running at ${res.url}`);
+});
